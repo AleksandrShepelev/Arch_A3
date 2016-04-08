83cb12acb9aa72cf6746fd6b84790814ea535949
atrantin <anton@mm94.ru>
2016-04-08 20:22:51 +0300

    fix for time message

---
+++ System C/Controllers/SecurityController.java
@@ -0,0 +1,60 @@
+package Controllers;
+
+import Framework.BaseController;
+import Framework.MessageProtocol;
+import Framework.TimeMessage;
+
+public class SecurityController extends BaseController{
+
+    private static final String CONTROLLER_NAME ="Security controller";
+
+    private SecurityController(String[] args) {
+        super(args,MessageProtocol.Type.SECURITY_ALARM);
+    }
+
+    public void main (String args[])
+    {
+        SecurityController controller = new SecurityController(args);
+        controller.execute();
+    }
+
+    @Override
+    protected String handleDeviceOutput(TimeMessage msg)
+    {
+        String msgBody = msg.getMessageText();
+        switch (msgBody) {
+            case MessageProtocol.Body.SECURITY_ALARM_ON:
+                _mw.WriteMessage("Security alarm is turned on!");
+                return MessageProtocol.Body.ACK_SECURITY_ALARM_ON;
+            case MessageProtocol.Body.SECURITY_ALARM_OFF:
+                _mw.WriteMessage("Security alarm is turned off!");
+                return MessageProtocol.Body.ACK_SECURITY_ALARM_OFF;
+            default:
+                _mw.WriteMessage("Unexpected message body in Security Controller: " + msgBody);
+                return null;
+        }
+    }
+
+    @Override
+    protected String getName()
+    {
+        return CONTROLLER_NAME;
+    }
+
+    @Override
+    protected String getType() {
+        return MessageProtocol.Body.REG_SECURITY_CONTROLLER;
+    }
+
+    @Override
+    protected float getWinPosX()
+    {
+        return 0.6f;
+    }
+
+    @Override
+    protected float getWinPosY()
+    {
+        return 0.0f;
+    }
+}
---
+++ System C/Framework/TimeMessage.java
@@ -0,0 +1,64 @@
+package Framework;
+
+import MessagePackage.Message;
+
+public class TimeMessage {
+
+    static final String BODY_DELIMETER = "_";
+
+    private Message _msg;
+
+    private long _timestamp;
+    private String _message;
+
+    public String getMessageText() {
+        return _message;
+    }
+
+    public int GetMessageId() {
+        return _msg.GetMessageId();
+    }
+
+    public Message getMessage() {
+        return _msg;
+    }
+
+    private void setMessageText(String msgText) {
+        _message = msgText;
+    }
+
+    public long getTimestamp() {
+        return _timestamp;
+    }
+
+    private void setTimestamp(long ts) {
+        _timestamp = ts;
+    }
+
+    public TimeMessage(int MsgId, String Text) {
+        _timestamp = System.currentTimeMillis();
+        _msg = new Message(MsgId, String.valueOf(_timestamp) + BODY_DELIMETER + Text);
+    }
+
+    public TimeMessage(Message msg) {
+        _msg = msg;
+        String body = msg.GetMessage();
+        String[] parts = body.split(BODY_DELIMETER);
+
+        //if it is a regular message just write it to the body
+        if (parts.length < 2) {
+            setMessageText(body);
+            return;
+        }
+
+        try {
+            long ts = Long.parseLong(parts[0]);
+            setMessageText(body.replaceAll(parts[0] + BODY_DELIMETER, ""));
+            _msg.SetSenderId(msg.GetSenderId());
+            setTimestamp(ts);
+        } catch (Exception e) {
+            System.out.println("Error parsing message body: " + body);
+            e.printStackTrace();
+        }
+    }
+}
